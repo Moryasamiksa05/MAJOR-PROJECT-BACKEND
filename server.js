f2ea4d1fc31156df57ed6ce82e359f623e3b7fbd
SamikshaMorya <moryasamiksha52@gmail.com>
2025-06-13 12:04:01 +0530

    modified

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const admin = require("./firebaseAdmin"); // Firebase Admin SDK
-const User = require("./models/User");
+const User = require("./models/user.js");
  // Correct path to your User model
 
 dotenv.config(); // Load environment variables
